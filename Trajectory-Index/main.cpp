1cc72c30e501224eaa40be8214c32e0671258c8a
dolaneast <neuzqd@gmail.com>
2015-11-07 00:47:45 +0800

    开始
    创建了一个OS命令行工程

---
+++ Trajectory-Index/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Trajectory-Index
+//
+//  Created by Dolan on 15/11/7.
+//  Copyright © 2015年 Dolan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
